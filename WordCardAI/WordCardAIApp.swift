eaf8fd848210fb809a765cf2b917001dd265a04e
YuyaFuruichi <shikuno0102@gmail.com>
2025-11-23 20:13:24 +0900

    Initial Commit

---
+++ WordCardAI/WordCardAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCardAIApp.swift
+//  WordCardAI
+//
+//  Created by YuyaFuruichi on 2025/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WordCardAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
